4d0e93476355730544dce2aa2277e9da47d4a857
vv4x <vvfourx@proton.me>
2022-06-18 13:19:22 +0800

    dynamic sizing

--- bottomtext.py
+++ bottomtext.py
@@ -2,12 +2,13 @@
 from PIL import Image, ImageFilter, ImageOps, ImageDraw, ImageFont
 from io import BytesIO
 
-rawtext = input('what bottomtext: ')
-path = 'kitty.png'
+#rawtext = input('what bottomtext: ')
+rawtext = 'moewew'
+path = 'kitty.jpg'
 
 text = rawtext.upper()
 
-textsize = 80
+
 
 image = Image.open(path)
 draw = ImageDraw.Draw(image)
@@ -15,10 +16,13 @@ draw = ImageDraw.Draw(image)
 with Image.open(path) as img:
     width, height = img.size
 
+textsize = (height // 10)
+strokesize = (height // 200)
+
 font = ImageFont.truetype('impact.ttf', size = textsize)
 
-draw.text((width // 2, height / 35),text, font = font, fill = (0,0,0), align = 'center', anchor = 'mt', stroke_width = 5, stroke_fill = (255,255,255))
-draw.text((width // 2, height - textsize ),'BOTTOM TEXT', font = font, fill = (0,0,0), align = 'center', anchor = 'mt', stroke_width = 5, stroke_fill = (255,255,255))
+draw.text((width // 2, height / 35),text, font = font, fill = (0,0,0), align = 'center', anchor = 'mt', stroke_width = strokesize, stroke_fill = (255,255,255))
+draw.text((width // 2, height - textsize ),'BOTTOM TEXT', font = font, fill = (0,0,0), align = 'center', anchor = 'mt', stroke_width = strokesize, stroke_fill = (255,255,255))
 
 image.save('bottomtext1.png')
 
