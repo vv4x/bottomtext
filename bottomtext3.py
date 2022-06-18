46b8e95a2219f185e5a9c57aa61b453420c85fae
vv4x <vvfourx@proton.me>
2022-06-18 12:59:54 +0800

    Bottomtext first

---
+++ bottomtext3.py
@@ -0,0 +1,25 @@
+#bottomtext3
+from PIL import Image, ImageFilter, ImageOps, ImageDraw, ImageFont
+from io import BytesIO
+
+rawtext = input('What bottomtext')
+path = 'kitty.png'
+
+text = rawtext.upper()
+
+textsize = 80
+
+image = Image.open(path)
+draw = ImageDraw.Draw(image)
+
+with Image.open(path) as img:
+    width, height = img.size
+
+font = ImageFont.truetype('impact.ttf', size = textsize)
+
+draw.text((width // 2, 0),text, font = font, fill = (0,0,0), align = 'center', anchor = 'mt', stroke_width = 5, stroke_fill = (255,255,255))
+draw.text((width // 2, height - textsize),'BOTTOMTEXT', font = font, fill = (0,0,0), align = 'center', anchor = 'mt', stroke_width = 5, stroke_fill = (255,255,255))
+
+image.save('bottomtext1.png')
+
+
